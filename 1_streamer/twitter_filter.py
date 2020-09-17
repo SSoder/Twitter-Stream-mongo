75e1ad0203f4144e8bbe9b557fa3cb9f00f9b287
ssoder <sodersg@gmail.com>
2020-09-17 12:09:43 -0400

    print to file changes

--- 1_streamer/twitter_filter.py
+++ 1_streamer/twitter_filter.py
@@ -2,6 +2,7 @@ import tweepy
 from textblob import TextBlob
 import datetime
 import json
+import sys
 
 
 file_path = '..\config\\twitter_creds.json'
@@ -22,21 +23,20 @@ api = tweepy.API(auth)
 class StreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-
-        if ('RT @' not in status.text):
-            tweets = 0
-            with open('..\output\\tweets.json', 'w') as outfile:
-                while tweets < 5:
-                    tweet_item = {
-                        'id_str' : status.id_str,
-                        'text' : status.text,
-                        'username' : status.user.screen_name,
-                        'name' : status.user.name,
-                        'received_at' : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    }
+        tweets = 0
+        while tweets < 5:
+            if ('RT @' not in status.text):
+                tweet_item = {
+                            'id_str' : status.id_str,
+                            'text' : status.text,
+                            'username' : status.user.screen_name,
+                            'name' : status.user.name,
+                            'received_at' : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        }            
+                with open('..\output\\tweets.json', 'w') as outfile:
                     json.dump(tweet_item, outfile)
-                    tweets += 1
-                
+            tweets += 1
+        sys.exit()    
                 
     def on_error(self, status_code):
         if status_code == 420:
