e1c8146fe5e658f9ebfb79f30c225943ed72a104
ssoder <sodersg@gmail.com>
2020-09-17 15:49:45 -0400

    folder structure made appropriate for Flask app

--- app/twatcher.py
+++ app/twatcher.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template
-from tweet_store import TweetStore
+#from tweet_store import TweetStore
 
 app = Flask(__name__)
 store = TweetStore()
-template = '..\\FrontEnd\\index.html'
 
 @app.route('/')
 def index():
     tweets = store.tweets()
-    return render_template(template, tweets = tweets)
+    return render_template('index.html', tweets = tweets)
 
 if __name__ == '__main__':
     app.run(debug=True)
